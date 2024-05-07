520fb033b8bec270e8eb92f23b78e476e934e174
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2024-05-07 18:11:31 +0800

    sm8250-common: device: Set TARGET_BOOTANIMATION_HALF_RES to true
    * No apparent resolution difference
    * Much smoother animation
    * Much faster boot
    * What's not to like?
    
    Change-Id: I2119fe826146a3103da50b4862ad88f5950a97ec

--- kona.mk
+++ kona.mk
@@ -145,6 +145,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Bootanimation
+TARGET_BOOTANIMATION_HALF_RES := true
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
