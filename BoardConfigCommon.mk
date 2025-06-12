768ff81123f0a1cbc5f9d2bbec80614a3da74568
1582130940 <19632674+1582130940@users.noreply.github.com>
2025-06-13 02:36:36 +0800

    sm8250-common: proprietary-files: Update blobs from pipa OS2.0.4.0.UMZMIXM

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -180,7 +180,7 @@ SOONG_CONFIG_XIAOMI_KONA_PARTITION_SCHEME := a
 endif
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2025-04-01
+VENDOR_SECURITY_PATCH := 2025-05-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
