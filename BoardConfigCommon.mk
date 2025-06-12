656597ef5038434b9a784d05b5816a1956cdbb79
Bruno Martins <bgcngm@gmail.com>
2025-06-12 22:22:47 +0800

    sm8250-common: device: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -102,9 +102,6 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8250
 KERNEL_CC := CC=clang
 override KERNEL_TOOLCHAIN_PREFIX_arm := arm-linux-android-
 
-# Lineage Health
-TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
-
 # Media
 TARGET_USES_ION := true
 
