2011d0fbc3f4482ac1116bc16b4d7067fdc09d0c
Sharath Chandra Vurukala <sharathv@codeaurora.org>
2025-07-20 13:00:10 +0800

    sm8250-common: BoardConfig: Enable raw_before_defrag
    enable raw_before_defrag from KERNEL_CMDLINE
    to let any iptable_raw and ip6table_raw modules
    register with a higher priority than conntrack.
    
    Change-Id: I6fb9ebd45daf917ee1c9d304cdfe80185b7505b8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -92,6 +92,7 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 an
 BOARD_KERNEL_CMDLINE += androidboot.fstab_suffix=qcom
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_CMDLINE += kpti=off
+BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1 ip6table_raw.raw_before_defrag=1
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_SEPARATED_DTBO := true
